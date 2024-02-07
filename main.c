a94c57da63fd70a98e9f4cb40fa00d02c3cfe69e
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-02-07 23:34:38 +0100

    [+] adding -h option to get help

--- main.c
+++ main.c
@@ -6,12 +6,28 @@
 */
 #include "include/my.h"
 #include "include/my_navy.h"
+#include "lib/my/my_printf/include/my_printf.h"
+
+static int help(void)
+{
+    my_printf("USAGE\n\t./my_navy [first_player_pid] navy_positions\n");
+    my_printf("DESCRIPTION\n\tfirst_player_pid: only for the 2nd player. %s",
+    "pid of the first player.\n");
+    my_printf("\tnavy_positions: %s",
+    "file representing the positions of the ships.\n");
+    return 0;
+}
 
 int main(int ac, char **av)
 {
-    char **map = get_map(av[1]);
-    char **enemy_map = create_void_map();
+    char **map;
+    char **enemy_map;
 
+    if (ac >= 2)
+        if (my_strncmp(av[1], "-h", 2) == 0)
+            return help();
+    enemy_map = create_void_map();
+    map = get_map(av[1]);
     if (map == NULL)
         return 84;
     if (ac == 3)
