461742a113c7a843e63ba4bbc815b636bb0b5001
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-02-07 23:34:46 +0100

    [~] add \n like the txt and not same of the pdf

--- ressources/connection.c
+++ ressources/connection.c
@@ -18,17 +18,17 @@ static void signal_handler(int signe)
 int connection_player1(void)
 {
     signal(SIGUSR1, signal_handler);
-    my_printf("my_pid: %d\n", getpid());
-    my_printf("waiting for enemy connection...\n");
+    my_printf("my_pid: %d\n\n", getpid());
+    my_printf("waiting for enemy connection...\n\n");
     while (!received_signal);
-    my_printf("enemy connected\n");
+    my_printf("enemy connected\n\n");
 }
 
 int send_signal_co(int pid)
 {
     if (kill(pid, SIGUSR1) == 0) {
-        my_printf("my_pid: %d\n", getpid());
-        my_printf("successfully connected\n");
+        my_printf("my_pid: %d\n\n", getpid());
+        my_printf("successfully connected to enemy\n\n");
     } else {
         return 84;
     }
